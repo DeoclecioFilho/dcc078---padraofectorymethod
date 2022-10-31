d99c0ede0a85e58be44ca7cc804e9650166e027e
Deoclécio Filho <deoclecio.filho@estudante.ufjf.br>
2022-10-31 15:12:35 -0300

    Caso de testes corrigido.

--- src/test/java/ServicoFactoryTest.java
+++ src/test/java/ServicoFactoryTest.java
@@ -9,7 +9,7 @@ public class ServicoFactoryTest {
     @Test
     void deveRetornarExcecaoParaServicoInexistente() {
         try {
-            IServico servico = ServicoFactory.obterServico("Concerto");
+            IServico servico = ServicoFactory.obterServico("Conserto");
             fail();
         } catch (IllegalArgumentException e) {
             assertEquals("Serviço inexistente", e.getMessage());
